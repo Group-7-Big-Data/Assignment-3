462195e5696587e270931aa0221f4236170a493b
sidthree6 <psiddhu001@gmail.com>
2020-07-18 15:47:56 -0400

    Added another CSV and dash app complete

--- app.py
+++ app.py
@@ -1,84 +1,116 @@
-# Loading Libraries
 import os
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 import pandas as pd
-import seaborn as sns
-
-import wordcloud
-
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn.svm import LinearSVC, SVC
-from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
-
-import joblib
-
-import re
-
-from bs4 import BeautifulSoup
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+stock = pd.read_csv('IBM_Stock_Price.csv')
+stock_train = pd.read_csv('IBM_Stock_Train.csv')
+stock_test_pred = pd.read_csv('IBM_Stock_Test_And_Predicted.csv')
+stock_vis = pd.read_csv('IBM_Stock_Month_Year_Date.csv')
+
 server = app.server
 
-# Loading dataset which has been cleaned in Jupyter Notebook
-dataset = pd.read_csv('https://raw.githubusercontent.com/Group-7-Big-Data/Assignment-2/master/IMDB_review_cleaned.csv')
-
-# Create a layout for user where they can enter sentence
 app.layout = html.Div([
-    html.H2('Movie review sentiment predictor:'),
-    html.P('Please write down a sentence'),
-    dcc.Input(id='sentence', value='', type='text', size='100'),
-    html.P(''),
-    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
-    html.P(''),
-    html.P('(Warning: This is slow due to heroku free version)'),
-    html.P('Prediction:'),
-    html.P(''),
-    html.Div(id='display-value')
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['Open/Close/High/Low Stock Prices', 'Average Price & Volume over Years', 'Average Price & Volume for each Months', 'Average Price & Volume for each days of month', 'Stock Prediction']],
+        value='Stock Prediction'
+    ),
+    dcc.Graph(id="graph_with_selection")
 ])
 
-# Creating a function which will yeild a prediction based on sentence entered by user.
-def predict_sentence(text, tfidf, svc, df):
-    text = BeautifulSoup(text).get_text()
-    text = re.sub('[^a-zA-Z]', ' ', text)
-    text = text.lower()
-    text = [text]
-    
-    tfid_transformed_text = tfidf.transform(text)
-    
-    y_pred = svc.predict(tfid_transformed_text)
-    
-    return y_pred
-
-# We have saved TF-IDF model and Linear SVC model in jupyter book, so model prediction is faster
-load_tfidf = joblib.load('tfidf.pkl')
-load_svc = joblib.load('linearsvc.pkl')
-
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('submit-button-state', 'n_clicks')],
-              [dash.dependencies.State('sentence', 'value')])    
-def display_value(n_clicks, value):
-    output = []
-    
-    # Show prediction when mouse is clicked first time and so on.
-    if n_clicks is not 0:        
-        prediction = predict_sentence(value, load_tfidf, load_svc, dataset)
-        output.append(html.P('Your sentence sentiment prediction is "{}"'.format(prediction[0])))
-        output.append(html.P('Sentence: {}'.format(value)))
-    
-    return output
-    
+@app.callback(dash.dependencies.Output('graph_with_selection', 'figure'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    if value == "Open/Close/High/Low Stock Prices":
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=stock['Date'], y=stock['Open'],
+                            mode='lines',
+                            name='Open Stock Price'))
+        fig.add_trace(go.Scatter(x=stock['Date'], y=stock['Close'],
+                            mode='lines',
+                            name='Close Stock Price'))
+        fig.add_trace(go.Scatter(x=stock['Date'], y=stock['High'],
+                            mode='lines',
+                            name='Close Stock Price'))
+        fig.add_trace(go.Scatter(x=stock['Date'], y=stock['Low'],
+                            mode='lines',
+                            name='Close Stock Price'))
+        fig.update_layout(title='Open / Close / High / Low stock prices of IBM over years',
+                   xaxis_title='Years',
+                   yaxis_title='IBM Stock Prices')
+        return fig
+    elif value == "Average Price & Volume for each Months":
+        
+        month_group = stock_vis.groupby('month', as_index=False)[['Open', 'Volume']].mean()
+        def toMonth(num):
+            num = num-1
+            month = ["January", "February", "March", "April", "May", "June", "July", "August",
+                     "September", "October", "November", "December"]
+            return month[num]
+        month_group['month'] = month_group['month'].apply(toMonth)
+        
+        fig = make_subplots(rows=2, cols=1)
+        fig.append_trace(go.Scatter(x=month_group['month'], y=month_group['Open'], 
+                            mode='lines',
+                            name='Average Open Stock Price') , row=1, col=1 )
+        fig.append_trace(go.Scatter(x=month_group['month'], y=month_group['Volume'], 
+                            mode='lines',
+                            name='Average Volumes') , row=2, col=1 )
+        fig.update_layout(height=900, title_text="Average Price & Volume for each Months")
+        return fig
+    elif value == "Average Price & Volume over Years":
+        
+        year_group = stock_vis.groupby('year', as_index=False)[['Open', 'Volume']].mean()
+        
+        fig = make_subplots(rows=2, cols=1)
+        fig.append_trace(go.Scatter(x=year_group['year'], y=year_group['Open'], 
+                            mode='lines',
+                            name='Average Open Stock Price') , row=1, col=1 )
+        fig.append_trace(go.Scatter(x=year_group['year'], y=year_group['Volume'], 
+                            mode='lines',
+                            name='Average Volumes') , row=2, col=1 )
+        fig.update_layout(height=900, title_text="Average Price & Volume over Years")
+        return fig
+    elif value == "Average Price & Volume for each days of month":
+        
+        day_group = stock_vis.groupby('day', as_index=False)[['Open', 'Volume']].mean()
+        
+        fig = make_subplots(rows=2, cols=1)
+        fig.append_trace(go.Scatter(x=day_group['day'], y=day_group['Open'], 
+                            mode='lines',
+                            name='Average Open Stock Price') , row=1, col=1 )
+        fig.append_trace(go.Scatter(x=day_group['day'], y=day_group['Volume'], 
+                            mode='lines',
+                            name='Average Volumes') , row=2, col=1 )
+        fig.update_layout(height=900, title_text="Average Price & Volume for each days of month")
+        return fig
+    else:
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=stock_train['Date'].tail(500), y=stock_train['Open'].tail(500),
+                            mode='lines',
+                            name='Training Stock Price'))
+        fig.add_trace(go.Scatter(x=stock_test_pred['Date'], y=stock_test_pred['Open'],
+                            mode='lines',
+                            name='Real Stock Price'))
+        fig.add_trace(go.Scatter(x=stock_test_pred['Date'], y=stock_test_pred['predicted_price'],
+                            mode='lines',
+                            name='Predicted Stock Price'))
+        fig.update_layout(height=900, title='Predicted Stock price of IBM',
+                   xaxis_title='Years',
+                   yaxis_title='IBM Stock Prices')
+        return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
